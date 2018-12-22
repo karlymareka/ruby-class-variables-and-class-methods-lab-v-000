0a5aabaf6aa26f7ec26ffd49d8efe9149a28c08d
Karly Mareka <karly.mareka@gmail.com>
2018-12-22 19:51:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,9 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
     @@count += 1
   end
   
